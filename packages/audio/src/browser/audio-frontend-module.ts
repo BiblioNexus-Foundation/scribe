e0a1ad5849d63eeef709558b9d8924b125e04f47
vipinpaul <vipin.paul@bridgeconn.com>
2025-02-07 14:03:02 +0530

    Made a new audio plugin widget

---
+++ packages/audio/src/browser/audio-frontend-module.ts
@@ -0,0 +1,16 @@
+import { ContainerModule } from '@theia/core/shared/inversify';
+import { AudioWidget } from './audio-widget';
+import { AudioContribution } from './audio-contribution';
+import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
+
+import '../../src/browser/style/index.css';
+
+export default new ContainerModule(bind => {
+    bindViewContribution(bind, AudioContribution);
+    bind(FrontendApplicationContribution).toService(AudioContribution);
+    bind(AudioWidget).toSelf();
+    bind(WidgetFactory).toDynamicValue(ctx => ({
+        id: AudioWidget.ID,
+        createWidget: () => ctx.container.get<AudioWidget>(AudioWidget)
+    })).inSingletonScope();
+});
