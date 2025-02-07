3d2818bb44c838b748488dcce08f8b2877521ff2
vipinpaul <vipin.paul@bridgeconn.com>
2025-02-07 14:03:02 +0530

    Made a new audio plugin widget

---
+++ packages/audio/src/browser/audio-widget.test.ts
@@ -0,0 +1,36 @@
+import 'reflect-metadata';
+import { MessageService } from '@theia/core';
+import { ContainerModule, Container } from '@theia/core/shared/inversify';
+import { AudioWidget } from './audio-widget';
+import { render } from '@testing-library/react'
+
+describe('AudioWidget', () => {
+
+    let widget: AudioWidget;
+
+    beforeEach(async () => {
+        const module = new ContainerModule( bind => {
+            bind(MessageService).toConstantValue({
+                info(message: string): void {
+                    console.log(message);
+                }
+            } as MessageService);
+            bind(AudioWidget).toSelf();
+        });
+        const container = new Container();
+        container.load(module);
+        widget = container.resolve<AudioWidget>(AudioWidget);
+    });
+
+    it('should render react node correctly', async () => {
+        const element = render(widget.render());
+        expect(element.queryByText('Display Message')).toBeTruthy();
+    });
+
+    it('should inject \'MessageService\'', () => {
+        const spy = jest.spyOn(widget as any, 'displayMessage')
+        widget['displayMessage']();
+        expect(spy).toBeCalled();
+    });
+
+});
