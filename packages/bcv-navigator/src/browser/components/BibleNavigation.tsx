4e341b4e9296a20546e011d6752d21c47ee26f8a
LijuJacob08 <liju.jacob08@gmail.com>
2025-02-17 17:26:55 +0530

    Issue41 - added Missing book

--- packages/bcv-navigator/src/browser/components/BibleNavigation.tsx
+++ packages/bcv-navigator/src/browser/components/BibleNavigation.tsx
@@ -32,7 +32,7 @@ const fullBibleScope: Scope = {
     GEN: [], EXO: [], LEV: [], NUM: [], DEU: [], JOS: [], JDG: [], RUT: [], 
     "1SA": [], "2SA": [], "1KI": [], "2KI": [], "1CH": [], "2CH": [], EZR: [], 
     NEH: [], EST: [], JOB: [], PSA: [], PRO: [], ECC: [], SNG: [], ISA: [], 
-    JER: [], LAM: [], EZK: [], DAN: [], HOS: [], JOE: [], AMO: [], OBA: [], 
+    JER: [], LAM: [], EZK: [], DAN: [], HOS: [], JOL: [], AMO: [], OBA: [], 
     JON: [], MIC: [], NAH: [], HAB: [], ZEP: [], HAG: [], ZEC: [], MAL: [],
     MAT: [], MRK: [], LUK: [], JHN: [], ACT: [], ROM: [], "1CO": [], "2CO": [],
     GAL: [], EPH: [], PHP: [], COL: [], "1TH": [], "2TH": [], "1TI": [], "2TI": [],
@@ -45,7 +45,7 @@ const fullBibleScope: Scope = {
 // Book name mapping
 const bookNameMapping: { [key: string]: string } = {
     // Old Testament
-    "GEN": "Genesis","EXO": "Exodus","LEV": "Leviticus","NUM": "Numbers",   "DEU": "Deuteronomy",    "JOS": "Joshua",    "JDG": "Judges",    "RUT": "Ruth",    "1SA": "1 Samuel",    "2SA": "2 Samuel",    "1KI": "1 Kings",    "2KI": "2 Kings",    "1CH": "1 Chronicles",    "2CH": "2 Chronicles",    "EZR": "Ezra",    "NEH": "Nehemiah",    "EST": "Esther",    "JOB": "Job",    "PSA": "Psalms",    "PRO": "Proverbs",    "ECC": "Ecclesiastes",    "SNG": "Song of Solomon",    "ISA": "Isaiah",    "JER": "Jeremiah",    "LAM": "Lamentations",    "EZK": "Ezekiel",    "DAN": "Daniel",    "HOS": "Hosea",    "JOE": "Joel",    "AMO": "Amos",    "OBA": "Obadiah",    "JON": "Jonah",    "MIC": "Micah",    "NAH": "Nahum",    "HAB": "Habakkuk",    "ZEP": "Zephaniah",    "HAG": "Haggai",    "ZEC": "Zechariah",    "MAL": "Malachi",
+    "GEN": "Genesis","EXO": "Exodus","LEV": "Leviticus","NUM": "Numbers",   "DEU": "Deuteronomy",    "JOS": "Joshua",    "JDG": "Judges",    "RUT": "Ruth",    "1SA": "1 Samuel",    "2SA": "2 Samuel",    "1KI": "1 Kings",    "2KI": "2 Kings",    "1CH": "1 Chronicles",    "2CH": "2 Chronicles",    "EZR": "Ezra",    "NEH": "Nehemiah",    "EST": "Esther",    "JOB": "Job",    "PSA": "Psalms",    "PRO": "Proverbs",    "ECC": "Ecclesiastes",    "SNG": "Song of Solomon",    "ISA": "Isaiah",    "JER": "Jeremiah",    "LAM": "Lamentations",    "EZK": "Ezekiel",    "DAN": "Daniel",    "HOS": "Hosea",    "JOL": "Joel",    "AMO": "Amos",    "OBA": "Obadiah",    "JON": "Jonah",    "MIC": "Micah",    "NAH": "Nahum",    "HAB": "Habakkuk",    "ZEP": "Zephaniah",    "HAG": "Haggai",    "ZEC": "Zechariah",    "MAL": "Malachi",
     // New Testament
     "MAT": "Matthew",    "MRK": "Mark",    "LUK": "Luke",    "JHN": "John",    "ACT": "Acts",    "ROM": "Romans",    "1CO": "1 Corinthians",    "2CO": "2 Corinthians",    "GAL": "Galatians",    "EPH": "Ephesians",    "PHP": "Philippians",    "COL": "Colossians",    "1TH": "1 Thessalonians",    "2TH": "2 Thessalonians",    "1TI": "1 Timothy",    "2TI": "2 Timothy",    "TIT": "Titus",    "PHM": "Philemon",    "HEB": "Hebrews",    "JAS": "James",    "1PE": "1 Peter",    "2PE": "2 Peter",   "1JN": "1 John",    "2JN": "2 John",    "3JN": "3 John",    "JUD": "Jude",    "REV": "Revelation"
 };
@@ -67,7 +67,7 @@ interface BibleStructure {
     "Old Testament": BibleBooks;
     "New Testament": BibleBooks;
 }
-const oldTestamentBooks = ["GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB", "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", "HOS", "JOE", "AMO", "OBA", "JON", "MIC", "NAH", "HAB", "ZEP", "HAG", "ZEC", "MAL"];
+const oldTestamentBooks = ["GEN", "EXO", "LEV", "NUM", "DEU", "JOS", "JDG", "RUT", "1SA", "2SA", "1KI", "2KI", "1CH", "2CH", "EZR", "NEH", "EST", "JOB", "PSA", "PRO", "ECC", "SNG", "ISA", "JER", "LAM", "EZK", "DAN", "HOS", "JOL", "AMO", "OBA", "JON", "MIC", "NAH", "HAB", "ZEP", "HAG", "ZEC", "MAL"];
 const newTestamentBooks = ["MAT", "MRK", "LUK", "JHN", "ACT", "ROM", "1CO", "2CO", "GAL", "EPH", "PHP", "COL", "1TH", "2TH", "1TI", "2TI", "TIT", "PHM", "HEB", "JAS", "1PE", "2PE", "1JN", "2JN", "3JN", "JUD", "REV"];
 
 
@@ -401,10 +401,10 @@ const BibleNavigation: React.FC<NavigationProps> = ({
             {dropdownOpen && (
                 <div className={`max-h-96 z-20 overflow-hidden p-2.5 border-2 border-blue-200 max-w-80
         ${isDarkMode ? 'bg-gray-900' : 'bg-white'}`}>
-                    <div className={`pl-4 font-semibold text-xl 
+                    {/* <div className={`pl-4 font-semibold text-xl 
             ${isDarkMode ? 'text-white' : 'text-black'}`}>
                         Go To
-                    </div>
+                    </div> */}
 
                     <div className="h-80 overflow-y-auto pt-0 pl-4 pb-4 pr-4" ref={dropdownRef}>
                         {Object.entries(bibleBooks).map(([testament, testamentBooks]: [string, BibleBooks]) => (
