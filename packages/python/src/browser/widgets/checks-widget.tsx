c25cd8e4084c3bdfea76e4ae631a7b0779347733
Brian Ineza <brianineza01@gmail.com>
2025-02-27 09:34:14 +0100

    ch: widget editable for checks

--- packages/python/src/browser/widgets/checks-widget.tsx
+++ packages/python/src/browser/widgets/checks-widget.tsx
@@ -39,10 +39,10 @@ export class ChecksWidget extends ReactWidget {
     this.title.iconClass = codicon("checklist");
     this.update();
 
-    this.getAnalysisData().then((data) => {
-      this.analysisData = data;
-      this.update();
-    });
+    // this.getAnalysisData().then((data) => {
+    //   this.analysisData = data;
+    //   this.update();
+    // });
   }
 
   protected async getAnalysisData(): Promise<AnalysisData> {
@@ -61,6 +61,14 @@ export class ChecksWidget extends ReactWidget {
   render(): React.ReactNode {
     return (
       <div className="">
+        <CheckInputForm
+          onSubmit={(text) => {
+            this.pythonService.executeWildebeest(text).then((data) => {
+              this.analysisData = JSON.parse(data);
+              this.update();
+            });
+          }}
+        />
         {this.analysisData && <TextAnalysis data={this.analysisData} />}
       </div>
     );
@@ -97,3 +105,49 @@ export class ChecksWidgetContribution
     });
   }
 }
+
+interface CheckInputFormProps {
+  onSubmit?: (text: string) => void;
+  placeholder?: string;
+  className?: string;
+}
+
+const CheckInputForm: React.FC<CheckInputFormProps> = ({
+  onSubmit,
+  placeholder = "Enter text to analyze...",
+  className,
+}) => {
+  const [inputText, setInputText] = React.useState<string>("");
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (inputText.trim() && onSubmit) {
+      onSubmit(inputText);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className={
+        "flex max-w-4/5 gap-2 p-2 border-t border-[rgb(250 250 250 / 0.1)] flex-col"
+      }
+    >
+      <label htmlFor="inputText">Enter text to analyze...</label>
+      <textarea
+        id="inputText"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder={placeholder}
+        rows={4}
+        className="flex w-full rounded-md border border-input bg-background px-3 py-2 text-xs ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 resize-none"
+      />
+      <button
+        type="submit"
+        className="dark:bg-cyan-500 bg-cyan-400 hover:bg-cyan-500 dark:hover:bg-cyan-400 text-zinc-800 dark:text-black dark:border-cyan-700"
+      >
+        Run Check
+      </button>
+    </form>
+  );
+};
