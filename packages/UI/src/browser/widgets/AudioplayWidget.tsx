d014fbceab736b3a2f5c1da6fba17a72c2f39c63
LijuJacob08 <liju.jacob08@gmail.com>
2025-03-18 12:58:43 +0530

    Issue41 rebased with development

--- packages/UI/src/browser/widgets/AudioplayWidget.tsx
+++ packages/UI/src/browser/widgets/AudioplayWidget.tsx
@@ -25,6 +25,8 @@ import {
 } from "@tabler/icons-react";
 import Button from "../../components/Button";
 import ButtonGroups from "../../components/ButtonGroup";
+import { PrevVerse } from 'bcv-navigator/lib/browser/components/PrevVerse';
+import { NextVerse } from 'bcv-navigator/lib/browser/components/NextVerse';
 
 @injectable()
 export class AudioPlayWidget extends ReactWidget {
@@ -143,63 +145,55 @@ export class AudioPlayWidget extends ReactWidget {
                     <IconTrashX size={14} stroke={2} strokeLinejoin="miter" />
                   }
                 />
-              </div>
-              <div className="space-y-4">
-                <p className="uppercase dark:text-zinc-500 text-zinc-400 text-[10px] text-center  font-medium ">
-                  Volume
-                </p>
-                <span className="flex items-center gap-x-2">
-                  <IconMinus
-                    size={14}
-                    stroke={2}
-                    strokeLinejoin="miter"
-                    className="cursor-pointer dark:text-zinc-50 text-zinc-700"
-                  />
-                  <span className="bg-white rounded-full h-2 w-40 border border-[rgb(250 250 250 / 0.1)] relative">
-                    <span className="bg-cyan-400 rounded-full h-2 w-[70%] absolute  -bottom-[1px] -left-[1px]"></span>
-                  </span>
-                  <IconPlus
-                    size={14}
-                    stroke={2}
-                    strokeLinejoin="miter"
-                    className="cursor-pointer dark:text-zinc-50 text-zinc-700"
-                  />
-                </span>
-              </div>
-            </div>
-            <div className="w-[1px] h-7 mt-auto bg-gray-300 dark:bg-zinc-700" />
-            <div className="2xl:w-[20%] w-[25%] flex flex-col gap-4 items-center ">
-              <span className="uppercase leading-3 dark:text-zinc-500 text-zinc-400 text-[10px]   font-medium ">
-                Takes
-              </span>
-              <div className="flex items-center gap-[10px]">
-                <Button
-                  className="dark:bg-green-500 rounded-full min-w-7 max-w-7 h-7 bg-green-400 hover:bg-green-500 dark:hover:bg-green-400 text-zinc-800 dark:text-zinc-50  dark:border-green-700"
-                  label="A"
-                />
-                <Button
-                  className="dark:bg-white border-cyan-400 rounded-full min-w-7 max-w-7 h-7 bg-white hover:bg-green-500  text-zinc-800 dark:text-black  dark:border-cyan-400"
-                  label="B"
-                />
-                <Button
-                  className="dark:bg-white border-cyan-400 rounded-full min-w-7 max-w-7 h-7 bg-white hover:bg-green-500  text-zinc-800 dark:text-black  dark:border-cyan-400"
-                  label="C"
-                />
-              </div>
-            </div>
-            <div className="w-[1px] h-7 mt-auto bg-gray-300 dark:bg-zinc-700" />
 
-            <div className="2xl:w-[10%] w-[15%] flex flex-col gap-4 items-center  ">
-              <p className="uppercase dark:text-zinc-500 text-zinc-400 text-[10px]   font-medium ">
-                Settings
-              </p>
-              <IconSettings
-                size={24}
-                stroke={2}
-                strokeLinejoin="miter"
-                className="dark:text-zinc-50 text-zinc-500"
-              />
+                <div className="space-y-4">
+                  <p className="uppercase dark:text-zinc-500 text-zinc-400 text-[10px] text-center  font-medium ">
+                    Volume
+                  </p>
+                  <span className="flex items-center gap-x-2">
+                    <IconMinus
+                      size={14}
+                      stroke={2}
+                      strokeLinejoin="miter"
+                      className="cursor-pointer dark:text-zinc-50 text-zinc-700"
+                    />
+                    <span className="bg-white rounded-full h-2 w-40 border border-[rgb(250 250 250 / 0.1)] relative">
+                      <span className="bg-cyan-400 rounded-full h-2 w-[70%] absolute  -bottom-[1px] -left-[1px]"></span>
+                    </span>
+                    <IconPlus
+                      size={14}
+                      stroke={2}
+                      strokeLinejoin="miter"
+                      className="cursor-pointer dark:text-zinc-50 text-zinc-700"
+                    />
+                  </span>
+                </div>
+              </div>
+              <div className="w-[1px] h-7 mt-auto bg-gray-300 dark:bg-zinc-700" >
+
+                <div className="w-[1px] h-7 mt-auto bg-gray-300 dark:bg-zinc-700" />
+
+                <div className="2xl:w-[10%] w-[15%] flex flex-col gap-4 items-center  ">
+                  <p className="uppercase dark:text-zinc-500 text-zinc-400 text-[10px]   font-medium ">
+                    Settings
+                  </p>
+                  <IconSettings
+                    size={24}
+                    stroke={2}
+                    strokeLinejoin="miter"
+                    className="dark:text-zinc-50 text-zinc-500"
+                  />
+                </div>
+
+              </div>
             </div>
+          </div>
+          <div className=" p-5 flex items-center justify-center ">
+
+            <PrevVerse />
+            <NextVerse />
+
+
           </div>
           <div className="p-5 flex items-center justify-end">
             <Badge className="h-4 max-h-4">saved 5 mins ago</Badge>
@@ -213,8 +207,7 @@ export class AudioPlayWidget extends ReactWidget {
 @injectable()
 export class AudioPlayContribution
   extends AbstractViewContribution<AudioPlayWidget>
-  implements FrontendApplicationContribution
-{
+  implements FrontendApplicationContribution {
   @inject(FrontendApplicationStateService)
   protected readonly stateService: FrontendApplicationStateService;
 
---
+++ packages/bcv-navigator/src/browser/bcv-navigator-frontend-module.ts
@@ -0,0 +1,16 @@
+import { ContainerModule } from '@theia/core/shared/inversify';
+import { BcvNavigatorWidget } from './bcv-navigator-widget';
+import { BcvNavigatorContribution } from './bcv-navigator-contribution';
+import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
+
+import "../output-tailwind.css";
+
+export default new ContainerModule(bind => {
+    bindViewContribution(bind, BcvNavigatorContribution);
+    bind(FrontendApplicationContribution).toService(BcvNavigatorContribution);
+    bind(BcvNavigatorWidget).toSelf();
+    bind(WidgetFactory).toDynamicValue(ctx => ({
+        id: BcvNavigatorWidget.ID,
+        createWidget: () => ctx.container.get<BcvNavigatorWidget>(BcvNavigatorWidget)
+    })).inSingletonScope();
+});
