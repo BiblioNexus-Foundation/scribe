dc3df41fd06560523635230113fdf67f84472e3f
Liju M Jacob <93033055+LijuJacob08@users.noreply.github.com>
2025-04-23 11:35:33 +0530

    bcv navigator using global state (#108)
    * Fix for App bundle issue (#87)
    
    * Added syncpack,linting and fixed bundle issue
    
    * Updated the scripts and removed the unwanted scripts
    
    * Updating Readme
    
    * removing the unwanted branch
    
    * adding retry strategy for mac build
    
    * changing the mac workflow
    
    * installing ripgrep
    
    * removing vscode/ripgrep
    
    * removing --verbose from mac
    
    * removing unused packages @theia/file-search and @theia/search-in-workspace
    
    * modifying the workflow for mac build
    
    * testing the mac workflow
    
    * cache cleaning
    
    * removing the test branch
    
    * Update package.json
    
    Updating the watch script
    
    * Feat: Fonts Menu drop down components (#45)
    
    Co-authored-by: tchami <131756888+tchamianest@users.noreply.github.com>
    
    * Revert "Feat: Fonts Menu drop down components (#45)" (#89)
    
    This reverts commit eb82e8f2c161a9f94b953042e7b5419633822fad.
    
    ---------
    
    Co-authored-by: ErnestTchami <131756888+ErnestTchami@users.noreply.github.com>
    Co-authored-by: tchami <131756888+tchamianest@users.noreply.github.com>
    Co-authored-by: Brian Ineza <brianineza01@gmail.com>
    
    * bcv navigator using global state
    
    * adding the missing dependecy
    
    * bcv navigator using global state
    
    * adding the missing dependecy
    
    ---------
    
    Co-authored-by: VIPIN PAUL <37212471+vipinpaul@users.noreply.github.com>
    Co-authored-by: ErnestTchami <131756888+ErnestTchami@users.noreply.github.com>
    Co-authored-by: tchami <131756888+tchamianest@users.noreply.github.com>
    Co-authored-by: Brian Ineza <brianineza01@gmail.com>
    Co-authored-by: Anumon@9910665292 <anumonachan9953@gmail.com>

--- packages/UI/src/browser/widgets/AudioplayWidget.tsx
+++ packages/UI/src/browser/widgets/AudioplayWidget.tsx
@@ -1,3 +1,213 @@
+// import * as React from "@theia/core/shared/react";
+// import { inject, injectable, postConstruct } from "@theia/core/shared/inversify";
+// import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
+// import {
+//   AbstractViewContribution,
+//   FrontendApplicationContribution,
+//   FrontendApplication,
+// } from "@theia/core/lib/browser";
+// import { FrontendApplicationStateService } from "@theia/core/lib/browser/frontend-application-state";
+// import { WorkspaceService } from "@theia/workspace/lib/browser";
+// import { Badge } from "../../components/ui/Badge";
+// import {
+//   IconMicrophone,
+//   IconMinus,
+//   IconPlayerPause,
+//   IconPlayerPlay,
+//   IconPlus,
+//   IconRefresh,
+//   IconSettings,
+//   IconTrashX,
+// } from "@tabler/icons-react";
+// import Button from "../../components/Button";
+// import ButtonGroups from "../../components/ButtonGroup";
+// import { PrevVerse } from "bcv-navigator/lib/browser/components/PrevVerse";
+// import { NextVerse } from "bcv-navigator/lib/browser/components/NextVerse";
+
+// import BibleNavigation from "bcv-navigator/lib/browser/components/BibleNavigation";
+// import { VerseRefUtils } from "@scribe/theia-utils/lib/browser";
+// import { VerseRefValue } from "bcv-navigator/lib/browser/components/BibleNavigation";
+
+// @injectable()
+// export class AudioPlayWidget extends ReactWidget {
+//   static readonly ID = "Audio-play-widget";
+//   static readonly LABER = "Audio";
+
+//   @postConstruct()
+//   protected init(): void {
+//     this.doInit();
+//   }
+
+//   protected async doInit(): Promise<void> {
+//     this.id = AudioPlayWidget.ID;
+//     this.title.label = AudioPlayWidget.LABER;
+//     this.title.caption = AudioPlayWidget.LABER;
+//     this.title.closable = true;
+//     this.update();
+//   }
+
+//   render(): React.ReactNode {
+//     return (
+//       <div className="">
+//         <div className="border-[rgb(250 250 250 / 0.1)] h-[30%] border-t">
+//           <ButtonGroups />
+//           {/* <IconAudio className="fill-zinc-100  w-[100vw]  dark:stroke-zinc-800 dark:fill-zinc-800 " /> */}
+
+//           <div className="flex">
+//             <div className="flex w-[20%] flex-col items-center gap-4 2xl:w-[15%]">
+//               <span className="text-[10px] font-medium uppercase leading-3 text-zinc-400 dark:text-zinc-500">
+//                 Audio
+//               </span>
+//               costom select
+//               {/* <CustomSelect
+//                 options={sources}
+//                 placeholder="Source"
+//                 triggerClassName="w-fit h-5 uppercase gap-1 text-[10px] bg-cyan-400 text-zinc-50 rounded-full"
+//               /> */}
+//             </div>
+//             <div className="mt-auto h-7 w-[1px] bg-gray-300 dark:bg-zinc-700" />
+//             <div className="flex w-[20%] flex-col items-center gap-4 2xl:w-[15%]">
+//               <span className="text-[10px] font-medium uppercase leading-3 text-zinc-400 dark:text-zinc-500">
+//                 Speed
+//               </span>
+//               speed select
+//               {/* <CustomSelect
+//                 options={speeds}
+//                 placeholder="2x"
+//                 triggerClassName="w-fit h-5 uppercase gap-1 text-[10px] bg-cyan-400 text-zinc-50 rounded-full"
+//               /> */}
+//             </div>
+//             <div className="mt-auto h-7 w-[1px] bg-gray-300 dark:bg-zinc-700" />
+//             <div className="flex w-[50%] justify-between gap-7 px-16 2xl:w-[40%]">
+//               <div className="space-y-2">
+//                 <p className="text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                   Rewind
+//                 </p>
+//                 <Button
+//                   className="rounded-lg"
+//                   icon={<IconRefresh size={14} stroke={2} strokeLinejoin="miter" />}
+//                 />
+//               </div>
+//               <div className="space-y-2">
+//                 <p className="text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                   Record
+//                 </p>
+//                 <Button
+//                   className="rounded-lg"
+//                   icon={<IconMicrophone size={14} stroke={2} strokeLinejoin="miter" />}
+//                 />
+//               </div>
+//               <div className="space-y-2">
+//                 <p className="text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                   Play
+//                 </p>
+//                 <Button
+//                   className="rounded-lg bg-cyan-400 text-zinc-800 hover:bg-cyan-500 dark:border-cyan-700 dark:bg-cyan-500 dark:text-zinc-50 dark:hover:bg-cyan-400"
+//                   icon={<IconPlayerPlay size={14} stroke={2} strokeLinejoin="miter" />}
+//                 />
+//               </div>
+//               <div className="space-y-2">
+//                 <p className="text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                   Pause
+//                 </p>
+//                 <Button
+//                   className="rounded-lg"
+//                   icon={<IconPlayerPause size={14} stroke={2} strokeLinejoin="miter" />}
+//                 />
+//               </div>
+//               <div className="space-y-2">
+//                 <p className="text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                   Delete
+//                 </p>
+//                 <Button
+//                   className="rounded-lg"
+//                   icon={<IconTrashX size={14} stroke={2} strokeLinejoin="miter" />}
+//                 />
+
+//                 <div className="space-y-4">
+//                   <p className="text-center text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                     Volume
+//                   </p>
+//                   <span className="flex items-center gap-x-2">
+//                     <IconMinus
+//                       size={14}
+//                       stroke={2}
+//                       strokeLinejoin="miter"
+//                       className="cursor-pointer text-zinc-700 dark:text-zinc-50"
+//                     />
+//                     <span className="border-[rgb(250 250 250 / 0.1)] relative h-2 w-40 rounded-full border bg-white">
+//                       <span className="absolute -bottom-[1px] -left-[1px] h-2 w-[70%] rounded-full bg-cyan-400"></span>
+//                     </span>
+//                     <IconPlus
+//                       size={14}
+//                       stroke={2}
+//                       strokeLinejoin="miter"
+//                       className="cursor-pointer text-zinc-700 dark:text-zinc-50"
+//                     />
+//                   </span>
+//                 </div>
+//               </div>
+//               <div className="mt-auto h-7 w-[1px] bg-gray-300 dark:bg-zinc-700">
+//                 <div className="mt-auto h-7 w-[1px] bg-gray-300 dark:bg-zinc-700" />
+
+//                 <div className="flex w-[15%] flex-col items-center gap-4 2xl:w-[10%]">
+//                   <p className="text-[10px] font-medium uppercase text-zinc-400 dark:text-zinc-500">
+//                     Settings
+//                   </p>
+//                   <IconSettings
+//                     size={24}
+//                     stroke={2}
+//                     strokeLinejoin="miter"
+//                     className="text-zinc-500 dark:text-zinc-50"
+//                   />
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="flex items-center justify-center p-5">
+//             <PrevVerse />
+//             <NextVerse />
+//           </div>
+//           <div className="flex items-center justify-end p-5">
+//             <Badge className="h-4 max-h-4">saved 5 mins ago</Badge>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// @injectable()
+// export class AudioPlayContribution
+//   extends AbstractViewContribution<AudioPlayWidget>
+//   implements FrontendApplicationContribution
+// {
+//   @inject(FrontendApplicationStateService)
+//   protected readonly stateService: FrontendApplicationStateService;
+
+//   @inject(WorkspaceService)
+//   protected readonly workspaceService: WorkspaceService;
+
+//   constructor() {
+//     super({
+//       widgetId: AudioPlayWidget.ID,
+//       widgetName: AudioPlayWidget.LABER,
+//       defaultWidgetOptions: {
+//         area: "bottom",
+//       },
+//     });
+//   }
+
+//   async onStart(app: FrontendApplication): Promise<void> {
+//     this.stateService.reachedState("ready").then(() => {
+//       this.openView({
+//         activate: true,
+//         reveal: true,
+//       });
+//     });
+//   }
+// }
+
 import * as React from "@theia/core/shared/react";
 import { inject, injectable, postConstruct } from "@theia/core/shared/inversify";
 import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
@@ -24,10 +234,18 @@ import ButtonGroups from "../../components/ButtonGroup";
 import { PrevVerse } from "bcv-navigator/lib/browser/components/PrevVerse";
 import { NextVerse } from "bcv-navigator/lib/browser/components/NextVerse";
 
+// import BibleNavigation from "bcv-navigator/lib/browser/components/BibleNavigation";
+import { VerseRefUtils } from "@scribe/theia-utils/lib/browser";
+// import { VerseRefValue } from "bcv-navigator/lib/browser/components/BibleNavigation";
+
 @injectable()
 export class AudioPlayWidget extends ReactWidget {
   static readonly ID = "Audio-play-widget";
   static readonly LABER = "Audio";
+
+  // Inject the VerseRefUtils service
+  @inject(VerseRefUtils)
+  protected readonly verseRefUtils!: VerseRefUtils;
 
   @postConstruct()
   protected init(): void {
@@ -49,6 +267,7 @@ export class AudioPlayWidget extends ReactWidget {
           <ButtonGroups />
           {/* <IconAudio className="fill-zinc-100  w-[100vw]  dark:stroke-zinc-800 dark:fill-zinc-800 " /> */}
 
+       
           <div className="flex">
             <div className="flex w-[20%] flex-col items-center gap-4 2xl:w-[15%]">
               <span className="text-[10px] font-medium uppercase leading-3 text-zinc-400 dark:text-zinc-500">
@@ -161,9 +380,20 @@ export class AudioPlayWidget extends ReactWidget {
             </div>
           </div>
           <div className="flex items-center justify-center p-5">
-            <PrevVerse />
-            <NextVerse />
+            {/* <PrevVerse />
+            <NextVerse /> */}
+            <PrevVerse verseRefUtils={this.verseRefUtils} />
+            <NextVerse verseRefUtils={this.verseRefUtils} />
           </div>
+             {/* Add BibleNavigation component here */}
+             {/* <div className="flex justify-center p-2">
+            <BibleNavigation
+              showPrevVerse={true}
+              showNextVerse={true}
+              verseRefUtils={this.verseRefUtils}
+            />
+          </div> */}
+
           <div className="flex items-center justify-end p-5">
             <Badge className="h-4 max-h-4">saved 5 mins ago</Badge>
           </div>
